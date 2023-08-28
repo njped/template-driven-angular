c6a48b8ee1a9822b221a146d7b89bdd74f650b1d
Nathan Pedigo <njpedigo@gmail.com>
2023-08-28 10:32:21 -0600

    created errors for not completing the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { FormsModule } from '@angular/forms';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,13 +21,15 @@ import { MatSelectModule } from '@angular/material/select';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatCardModule,
     MatFormFieldModule,
+    MatInputModule,
     MatCheckboxModule,
     MatRadioModule,
     MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Employee } from '../employee';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.css']
 })
-export class EmployeeFormComponent {
+export class EmployeeFormComponent implements OnInit{
+  model = new Employee(
+    'John',
+    'Doe',
+    true,
+    'W2'
+    );
 
+    @ViewChild('empForm') empForm: NgForm | any
+
+    ngOnInit(): void {
+      console.log(`Employee's first name is ${this.model.firstName}`)
+      console.log(`Employee's last name is ${this.model.lastName}`)
+    }
+
+    submitForm() {
+      console.log('form submitted');
+      console.log(JSON.stringify(this.model))
+      console.log(this.empForm.valid);
+    }
 }
